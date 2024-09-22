cb1779d5fa8dedfcb38faea6b3feeb2663cd7479
zhangyu <2498623825@qq.com>
2024-09-22 15:50:37 +0800

    初始化项目

---
+++ aggregatecloud-admin/src/main/java/org/kele/aggregatecloud/AggregateCloudApplication.java
@@ -0,0 +1,13 @@
+package org.kele.aggregatecloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggregateCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AggregateCloudApplication.class, args);
+	}
+
+}
---
+++ aggregatecloud-common/src/test/java/org/kele/aggregatecloudcommon/AggregatecloudCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kele.aggregatecloudcommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AggregatecloudCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
