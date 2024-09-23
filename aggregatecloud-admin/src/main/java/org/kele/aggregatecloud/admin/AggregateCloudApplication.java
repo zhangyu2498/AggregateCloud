1d6a89c20a3e282878c8af01a2c663b81f284e30
zhangyu <2498623825@qq.com>
2024-09-23 09:06:58 +0800

    依赖修改

--- aggregatecloud-admin/src/main/java/org/kele/aggregatecloud/admin/AggregateCloudApplication.java
+++ aggregatecloud-admin/src/main/java/org/kele/aggregatecloud/admin/AggregateCloudApplication.java
@@ -1,9 +1,11 @@
-package org.kele.aggregatecloud;
+package org.kele.aggregatecloud.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "org.kele.*")
 public class AggregateCloudApplication {
 
 	public static void main(String[] args) {
